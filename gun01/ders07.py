e68223789d4ca3f656d61d9bb90d6e751a995c69
Sinan URUN <sinanurun24@gmail.com>
2025-01-27 15:03:30 +0300

    ders07

---
+++ gun01/ders07.py
@@ -0,0 +1,16 @@
+# matematiksel işlemler için artimetiksel operatörler kullanılır
+# aritmetiksel operatörler sayılarla kullanılır
+print(38 * 9) # * operatörü çarpma için kullanılır
+print(2.5 * 8)
+print(38 / 9)
+print(8/2)
+
+print(5+8)
+
+#stringlerde kullanılan opeartörler
+print("asena" * 5)
+# print("asena" * "mutlu")
+print("asena" + "mutlu")
+print("asena" + "mutlu" + "8")
+
+# * opeartörü çoğaltır, + operatörü birleştirir
