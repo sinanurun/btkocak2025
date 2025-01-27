ca6cea9f6242ab9cf29a5985c91bf034552214f3
Sinan URUN <sinanurun24@gmail.com>
2025-01-27 16:45:56 +0300

    Update ders14.py

--- gun01/ders14.py
+++ gun01/ders14.py
@@ -1,2 +1,21 @@
 #tip dönüşümü
 #işlem için en önemli şey dönüştürülebilir olması verinin
+sayi = 15
+print(sayi*2, type(sayi))
+sSayi = str(sayi)
+print(sSayi*2, type(sSayi))
+# tamIsım tam_isim tamisim firstName lastNAme last_name
+s_ondalikli = "17.5"
+ondalikli = float(s_ondalikli)
+print(ondalikli*2, type(ondalikli))
+
+smantik = "False"
+mantiksal = bool(smantik)
+print(mantiksal, type(mantiksal))
+
+smantik = "fsdfsdf"
+mantiksal = bool(smantik)
+print(mantiksal, type(mantiksal))
+
+smantik = str(mantiksal)
+print(smantik, type(smantik))
