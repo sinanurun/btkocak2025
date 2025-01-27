585ce70f2348d5b9622a2483c146820835be12cd
Sinan URUN <sinanurun24@gmail.com>
2025-01-27 16:12:03 +0300

    wer

---
+++ gun01/ders11.py
@@ -0,0 +1,8 @@
+ad = "gökhan"
+print(ad)
+print(type(ad))
+sayi = 5
+print(sayi)
+print(type(sayi))
+print(sayi == 8)
+print(sayi * 2)
---
+++ gun01/ders12.py
@@ -0,0 +1,9 @@
+isim = "Defne"
+soyisim = "Turgut"
+tam = isim + " " + soyisim
+print(tam)
+print("adım", isim, "soyadım", soyisim, "tam adım", tam)
+boy = 1.57
+yas = 23
+durum = True
+print("adım", isim, "soyadım", soyisim, "tam adım", tam,"boyum",boy, "durum", durum)
