c32f3a8bb649764cb06b25d0eb1c5c42384c2576
Sinan URUN <sinanurun24@gmail.com>
2025-01-27 14:20:31 +0300

    Create ders02.py

---
+++ gun01/ders02.py
@@ -0,0 +1,17 @@
+"""
+Temel veri tipleri
+tam sayı -> integer int()  85
+ondalikli sayi -> float float() 1.77
+
+
+"""
+# aşağıda sayı veri tipi için örnekler verilmiştir
+print(85)
+print(1.77)
+#bir verinin tipini öğrenmek için type() fonk kullanılır
+# type(x) => x'in veri tipini verir veya döndürür
+print(type(85))
+print(type(1.77))
+
+print(85.0)
+print(type(85.0))
