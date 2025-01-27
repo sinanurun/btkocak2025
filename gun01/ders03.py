f40eed7defa52da838ec06ede7be61fe809123b0
Sinan URUN <sinanurun24@gmail.com>
2025-01-27 14:32:37 +0300

    Create ders03.py

---
+++ gun01/ders03.py
@@ -0,0 +1,17 @@
+# metinsel veri tipi -> string str
+# string tnaımlamak için "x" , 'x', """x"""
+print("btk akademi")
+print('btk akademi')
+print("""btk akademi""")
+# üç tırnaklı yapıyı çok satırlı str tanımlamak için kullanabiliriz
+print("""btk akademi
+      izmir ili
+      üni öğrencilerine yönelik
+      python kursu""")
+print("8")
+print(type("8"))
+#kaçış operatörleri genel olarak \ ile kullanılır
+print("bu kurs \"izmir\" ilinde düzenleniyor")
+print("bu kurs 'izmir' ilinde düzenleniyor")
+print("bu kurs \tizmir ilinde düzenleniyor")
+print("bu kurs \nizmir ilinde düzenleniyor")
