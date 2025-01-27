5f60c81dc027b195391a0b5c2bcc280e6019b2e0
Sinan URUN <sinanurun24@gmail.com>
2025-01-27 15:39:53 +0300

    Create ders09.py

---
+++ gun01/ders09.py
@@ -0,0 +1,22 @@
+#karşılaştırma operatörleri
+#genel olarak sayılarda kullanılır
+#stringler vb yapılarda eşitliği kontrol amacıyla kullanılır
+# işlem sonuçları her zaman mantıksal olur yani True - False
+# eşitlik kontrolü için ==
+print(9 == 8)
+print(5 == 5)
+print("ali" == "ayşe")
+print("bora" == "bora")
+#eşitsizlik kontrolü için !=
+print(9 != 9)
+print("ali" != "ayşe")
+print(True == True)
+print("8" == 8)
+
+print(5 < 6)
+print( 5 <= 6)
+
+print(5 >= 8)
+print(5 > 8)
+
+print("ehliyet alma durumunuz", 12 >= 18)
