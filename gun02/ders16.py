2250845e214e53c47b08c48fcb5902965b06adc1
Sinan URUN <sinanurun24@gmail.com>
2025-01-28 16:44:39 +0300

    Update ders16.py

---
+++ gun02/ders16.py
@@ -0,0 +1,13 @@
+print(list(range(10)))
+for dd in range(1,6): #(baslangic:bitis:artis)
+    print(dd,". pizzanız")
+
+for harf in "bengisu":
+    print(harf)
+
+for dd in range(2,25,3):
+    print(dd)
+
+ad = "zeynepbengisu"
+for dd in ad[5:]:
+    print(dd, end="-")
