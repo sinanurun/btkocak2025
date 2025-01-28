0d585285c9aae6a921f65e2ba1a7a5bbc760fc98
Sinan URUN <sinanurun24@gmail.com>
2025-01-28 16:22:22 +0300

    d

--- gun02/ders14.py
+++ gun02/ders14.py
@@ -17,3 +17,6 @@ print(metin2.format(ad2, ad3, ad1))
 
 metin3 = "{k1},Merhaba {k1}, {k2}, {k3}".format(k1=ad1, k2=ad2, k3=ad3)
 print(metin3)
+print(metin3.capitalize())
+print(metin3.count("an"))
+print(metin3.count("an"))
---
+++ gun02/ders15.py
@@ -0,0 +1,8 @@
+#print() parametreleri
+print(help(print))
+print("cansel","büşra","barış")
+print("cansel","büşra","barış", sep="*")
+print("cansel","büşra","barış", end="-")
+print("cansel","büşra","barış")
+print("cansel","büşra","barış",sep="---",end="\t")
+print("cansel","büşra","barış")
