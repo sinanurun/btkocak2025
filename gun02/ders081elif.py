6ce039a57e25a76186844ad2eefd78b2483d074e
Sinan URUN <sinanurun24@gmail.com>
2025-01-28 13:45:43 +0300

    Create ders081elif.py

---
+++ gun02/ders081elif.py
@@ -0,0 +1,11 @@
+puan = int(input("ortalama puanınızı giriniz"))
+if puan < 0:
+    print("0 dan küçük puan girişi yaptınız hatalı")
+elif puan < 70:
+    print("belge için yeterli puanınız yok")
+elif puan < 85:
+    print("teşekkür belgesi aldınız tebrikler")
+elif puan <=100:
+    print("takdir alabilirsiniz tebrikler")
+else:
+    print("geçerli puan girişi sağlanamadı")
