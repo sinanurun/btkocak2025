6bed3a364bafc6f0eb797d8c8b99cb5f0e4cefc2
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 16:43:51 +0300

    Create ders09.py

---
+++ gun03/ders09.py
@@ -0,0 +1,14 @@
+treng = {"araba":"car","kalem":"pencil","kırmızı":"red"}
+print(treng)
+print(treng["araba"])
+
+print(treng.values())
+print(treng.keys())
+print(treng.items())
+
+for dd in treng:
+    print(dd, treng[dd])
+
+for key, value in treng.items():
+    print(key, "=>", value)
+
