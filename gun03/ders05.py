29defbfac6d49d54d119521e2c10f26987e52618
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 14:38:14 +0300

    Create ders05.py

---
+++ gun03/ders05.py
@@ -0,0 +1,24 @@
+adet = int(input("kaç tane zerzevat alacaksın"))
+zerzevat = []
+for i in range(adet):
+    print(i+1,". ne alacaksın")
+    urun = input("urun: ")
+    zerzevat.append(urun)
+
+for urun in zerzevat:
+    print(urun)
+
+#versiyon 2 pazar listesi
+mesaj = """Pazar Listesi Oluşturma Programına Hoşgeldiniz
+Programdan Çıkmak için X'e basınız"""
+print(mesaj)
+zerzevat = []
+while True:
+    urun = input("Ürün girişi yapınız")
+    if urun.lower() == "x":
+        print("pazar listeniz", zerzevat)
+        break
+    elif urun == "":
+        continue
+    else:
+        zerzevat.append(urun)
