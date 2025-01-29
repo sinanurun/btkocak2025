5d2a118aff394b9d4bec3bb9cedb450f53c9f357
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 15:05:18 +0300

    Update ders06.py

--- gun03/ders06.py
+++ gun03/ders06.py
@@ -7,11 +7,14 @@ yesillik = ["maydanoz", "roka","semiz otu"]
 # print(len(pazar_listesi))
 pazar_listesi = [sebze, meyve, sark, yesillik]
 print(pazar_listesi)
+pazar_listesi.append("baklava")
 print(len(pazar_listesi))
 print(pazar_listesi[0])
 print(pazar_listesi[0][1])
 print(pazar_listesi[0][0])
 for kategori in pazar_listesi:
     print(kategori)
+    if type(kategori) != list:
+        continue
     for urun in kategori:
         print("\t",urun)
