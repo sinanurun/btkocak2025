3dd8728941b5e5c206e5a827b650393175d978e9
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 14:08:36 +0300

    j

---
+++ gun03/ders02.py
@@ -0,0 +1,2 @@
+x = list()
+print(x)
---
+++ gun03/ders03.py
@@ -0,0 +1,7 @@
+ad = "edazeynep"
+adHarfleri = list(ad)
+print(adHarfleri)
+print(len(adHarfleri))
+yeni_string = str(adHarfleri)
+print(yeni_string)
+print(len(yeni_string))
