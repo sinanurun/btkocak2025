006de30761a1c517f93fcc89e61968ec9c627fbb
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 16:26:48 +0300

    q

---
+++ gun03/ders07.py
@@ -0,0 +1,7 @@
+kisisel = "zeynep","ışıklar"
+print(kisisel, type(kisisel))
+kisisel2 = ("zeynep",)
+print(kisisel2, type(kisisel2))
+
+print(kisisel.count("zeynep"))
+print(kisisel, type(kisisel))
---
+++ gun03/ders08.py
@@ -0,0 +1,11 @@
+ogrenciler = {"ali","veli","can","kaan"}
+print(ogrenciler)
+ogrenciler.add("kamil")
+print(ogrenciler)
+ogrenciler.add("can")
+print(ogrenciler)
+if "can" in ogrenciler:
+    print("can kümede")
+
+for i in ogrenciler:
+    print(i)
