3a97a24d4f0fc38f7663a94f9657801ac324a0a7
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 11:41:25 +0300

    t

---
+++ gun02/ders22.py
@@ -0,0 +1,12 @@
+#yontem 1
+# cevap = input("pizza ister misiniz")
+# while cevap == "evet":
+#     print("buyrun pizzanız")
+#     cevap = input("pizza ister misin")
+
+print("---"*20)
+#yontem 2
+cevap = bool(input("pizza ister misiniz"))
+while cevap == True: #cevap
+    print("buyrun pizzanız")
+    cevap = bool(input("pizza ister misiniz"))
