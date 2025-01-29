ff2ab063d550e50864be2734761fc1f7a1e07740
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 11:58:28 +0300

    a

--- gun02/ders23.py
+++ gun02/ders23.py
@@ -1,3 +1,18 @@
 # if True:
 #     pass
+"""
+for dd in range(25):
+    ad = input("Enter name")
+    if ad == "yemre":
+        print("yemre bulundu")
+        break
+"""
+
+for dd in range(25):
+    ad = input("Enter name")
+    if ad == "yemre":
+        print("yemre kayıt edilemez")
+        continue
+    soyad = input("Enter surname")
+    print(f"{ad} {soyad} sisteme kaydedildi")
 
---
+++ gun02/ders24.py
@@ -0,0 +1,7 @@
+carpim = 1
+while True:
+    sayi = int(input("bir sayı giriniz"))
+    if not(sayi == 0):
+        carpim = carpim*sayi
+    else:
+        break
