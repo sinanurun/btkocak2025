cfcbc96dcabada9ed74692d1fb4536cc7296062f
Sinan URUN <sinanurun24@gmail.com>
2025-01-29 10:41:32 +0300

    Update ders19.py

--- gun02/ders19.py
+++ gun02/ders19.py
@@ -1,5 +1,12 @@
 baslangic = 2
 bitis = 15
 artis = 3
-for dd in range(baslangic,bitis,artis):
-    print(dd)
+toplam = 0
+carpim = 1
+for dd in range(1,bitis+1):
+    toplam = toplam + dd
+    carpim = carpim * dd
+    print(dd,toplam)
+    print(dd,carpim)
+
+print(toplam)
