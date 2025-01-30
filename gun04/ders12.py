e7649e8de5ac800ebbdde49cc3f6b102133fcd3a
Sinan URUN <sinanurun24@gmail.com>
2025-01-30 16:05:26 +0300

    Update ders12.py

--- gun04/ders12.py
+++ gun04/ders12.py
@@ -2,17 +2,22 @@ def toplama(a,b):
     return a + b
 def fark(a,b):
     return a- b
+def islemYap(islem,sayi1,sayi2):
+    match islem:
+        case "+":
+            print(toplama(sayi1, sayi2))
+        case "-":
+            print(fark(sayi1, sayi2))
+        case _:
+            print("hatali işlem seçimi")
 def karsilama():
     print("programa hosgeldin")
     islem = input("+,-")
-    sayi1 = int(input("sayi1 giriniz: "))
-    sayi2 = int(input("sayi2 giriniz: "))
-    match islem:
-        case "+":
-            print(toplama(sayi1,sayi2))
-        case "-":
-            print(fark(sayi1,sayi2))
-        case _ :
-            print("hatali işlem seçimi")    
+    def sayiAl():
+        return int(input("sayi giriniz: "))
+    sayi1 = sayiAl()
+    sayi2 = sayiAl()
+    islemYap(islem,sayi1,sayi2)
+
 if __name__=="__main__":
     karsilama()
